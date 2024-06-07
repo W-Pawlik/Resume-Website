c57302d73a700e2cd183d6c93b4ddeca8ef55646
Wojtek Pawlik <wojtek.pawlik17@gmail.com>
2024-06-08 01:29:20 +0200

    Implemented Scroll Behaviour and Sticky Nav
    Implemented Scroll Behaviour and Sticky Navigation using js. Created rest of media queries for phones.

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,16 +1,64 @@
 'use strict';
 
-const menu_btn = document.querySelector('.btn-mobile-nav');
-const header = document.querySelector('.header');
+// NAVIGATION
+const menuBtnEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
 
-menu_btn.addEventListener('click', e => {
-  if (header.classList.contains('nav-open')) {
-    header.classList.remove('nav-open');
+menuBtnEl.addEventListener('click', e => {
+  if (headerEl.classList.contains('nav-open')) {
+    headerEl.classList.remove('nav-open');
     document.getElementsByTagName('body')[0].style.overflowY = 'hidden';
     document.getElementsByTagName('html')[0].style.overflowY = 'scroll';
   } else {
-    header.classList.add('nav-open');
+    headerEl.classList.add('nav-open');
     document.getElementsByTagName('body')[0].style.overflowY = 'hidden';
     document.getElementsByTagName('html')[0].style.overflowY = 'hidden';
   }
 });
+
+// SCROLL BEHAVIOUR
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+    console.log(href);
+
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    if (link.classList.contains('nav-link')) {
+      headerEl.classList.toggle('nav-open');
+      document.getElementsByTagName('body')[0].style.overflowY = 'hidden';
+      document.getElementsByTagName('html')[0].style.overflowY = 'scroll';
+    }
+  });
+});
+
+// STICKY NAV
+
+const sectionPrimeEl = document.querySelector('.section-prime');
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) document.body.classList.add('sticky');
+    if (ent.isIntersecting) document.body.classList.remove('sticky');
+  },
+  {
+    root: null,
+    treshold: 0,
+    rootMargin: '-100px',
+  }
+);
+observer.observe(sectionPrimeEl);

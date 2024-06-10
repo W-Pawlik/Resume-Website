5e10ec7e927f4f9af323f2680f753a9ca1304345
Wojtek Pawlik <wojtek.pawlik17@gmail.com>
2024-06-10 18:19:41 +0200

    Created popUps. Popups show data from json file which contains data such as id, description, title. Js script fetch data and checks if id of event listener element is the same as id in json file and take specific data for that number.

--- resources/js/index.js
+++ resources/js/index.js
@@ -62,3 +62,76 @@ const observer = new IntersectionObserver(
   }
 );
 observer.observe(sectionPrimeEl);
+
+
+// MY STACK POP UPS
+
+const popUpEl = document.querySelector('.pop-up');
+const popUpOpenerEl = document.querySelectorAll('.pop-up-opener');
+const overlayEl = document.querySelector('.overlay')
+const closeBtnPopUpEl = document.querySelector('.close-btn')
+
+
+popUpOpenerEl.forEach(function(popup){
+  popup.addEventListener("click", (e) => {
+    popUpEl.classList.add("open");
+    overlayEl.classList.add('active')
+    console.log(popup)
+
+    const popupId = e.target.id;
+    console.log(popupId)
+
+    fetch('./resources/data/myStack.json').then(function(response){
+      return response.json();
+    }).then(function(listOfStack){
+      console.log(listOfStack)
+      const placeholder = document.querySelector('.pop-up');
+    
+      let out = "";
+      for(let stack of listOfStack){
+        console.log(stack.id)
+        if (popupId === stack.id.toString()){
+          console.log(`hej ${stack.id} ${stack.title}`)
+          out += `
+          <div class="pop-up-top">
+          <h2 class="heading-primary pop-up-title">${stack.title}</h2>
+          <i class="fa-solid fa-xmark close-btn"></i>
+        </div>
+       
+        <p class="pop-up-description">${stack.description}</p>
+          `
+          placeholder.innerHTML = out;
+          break;
+        } 
+      
+        }
+
+        // console.log(stack.title)
+        // console.log(stack.description)
+      //   out += `
+      //   <div class="pop-up-top">
+      //   <h2 class="heading-primary pop-up-title">${stack.title}</h2>
+      //   <i class="fa-solid fa-xmark close-btn"></i>
+      // </div>
+     
+      // <p class="pop-up-description">${stack.description}</p>
+      //   `
+      // }
+    
+      // placeholder.innerHTML = out;
+    })
+  })
+})
+
+
+const closePopUp = () =>{
+  popUpEl.classList.remove("open")
+  overlayEl.classList.remove('active')
+}
+
+overlayEl.addEventListener('click', closePopUp)
+closeBtnPopUpEl.addEventListener('click',closePopUp)
+
+// MY STACK DATA
+
+
